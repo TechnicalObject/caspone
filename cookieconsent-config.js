9093567265002fe0b4325281a66b56d1e5d19b53
Luboš Komjati <79003016+TechnicalObject@users.noreply.github.com>
2024-02-06 23:18:26 +0100

    external languages

--- cookieconsent-config.js
+++ cookieconsent-config.js
@@ -4,6 +4,20 @@
  */
 import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@v3.0.0/dist/cookieconsent.umd.js';
 
+function consentUpdate(cookie) {
+    let consentLevel = cookie["level"];
+    let adStorage = consentLevel.includes("targeting") ? "granted" : "denied";
+    let analyticsStorage = consentLevel.includes("analytics") ? "granted" : "denied";
+    let adUserData = consentLevel.includes("targeting") ? "granted" : "denied";
+    let adPersonalization = consentLevel.includes("targeting") ? "granted" : "denied";
+    gtag('consent', 'update', {
+        ad_storage: adStorage,
+        analytics_storage: analyticsStorage,
+        ad_user_data: adUserData,
+        ad_personalization: adPersonalization
+    });
+}
+
 CookieConsent.run({
 
     // root: 'body',
@@ -41,11 +55,13 @@ CookieConsent.run({
     },
 
     onConsent: ({cookie}) => {
-        console.log('onConsent fired!', cookie)
+        console.log('onConsent fired!', cookie);
+        consentUpdate(cookie);
     },
 
     onChange: ({changedCategories, changedServices}) => {
         console.log('onChange fired!', changedCategories, changedServices);
+        consentUpdate(cookie);
     },
 
     onModalReady: ({modalName}) => {
@@ -95,77 +111,10 @@ CookieConsent.run({
     },
 
     language: {
-        default: 'en',
+        default: 'cs',
         translations: {
-            en: {
-                consentModal: {
-                    title: 'We use cookies',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-                    acceptAllBtn: 'Accept all',
-                    acceptNecessaryBtn: 'Reject all',
-                    showPreferencesBtn: 'Manage Individual preferences',
-                    // closeIconLabel: 'Reject all and close modal',
-                    footer: `
-                        <a href="#path-to-impressum.html" target="_blank">Impressum</a>
-                        <a href="#path-to-privacy-policy.html" target="_blank">Privacy Policy</a>
-                    `,
-                },
-                preferencesModal: {
-                    title: 'Manage cookie preferences',
-                    acceptAllBtn: 'Accept all',
-                    acceptNecessaryBtn: 'Reject all',
-                    savePreferencesBtn: 'Accept current selection',
-                    closeIconLabel: 'Close modal',
-                    serviceCounterLabel: 'Service|Services',
-                    sections: [
-                        {
-                            title: 'Your Privacy Choices',
-                            description: `In this panel you can express some preferences related to the processing of your personal information. You may review and change expressed choices at any time by resurfacing this panel via the provided link. To deny your consent to the specific processing activities described below, switch the toggles to off or use the “Reject all” button and confirm you want to save your choices.`,
-                        },
-                        {
-                            title: 'Strictly Necessary',
-                            description: 'These cookies are essential for the proper functioning of the website and cannot be disabled.',
-
-                            //this field will generate a toggle linked to the 'necessary' category
-                            linkedCategory: 'necessary'
-                        },
-                        {
-                            title: 'Performance and Analytics',
-                            description: 'These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.',
-                            linkedCategory: 'analytics',
-                            cookieTable: {
-                                caption: 'Cookie table',
-                                headers: {
-                                    name: 'Cookie',
-                                    domain: 'Domain',
-                                    desc: 'Description'
-                                },
-                                body: [
-                                    {
-                                        name: '_ga',
-                                        domain: location.hostname,
-                                        desc: 'Description 1',
-                                    },
-                                    {
-                                        name: '_gid',
-                                        domain: location.hostname,
-                                        desc: 'Description 2',
-                                    }
-                                ]
-                            }
-                        },
-                        {
-                            title: 'Targeting and Advertising',
-                            description: 'These cookies are used to make advertising messages more relevant to you and your interests. The intention is to display ads that are relevant and engaging for the individual user and thereby more valuable for publishers and third party advertisers.',
-                            linkedCategory: 'ads',
-                        },
-                        {
-                            title: 'More information',
-                            description: 'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>'
-                        }
-                    ]
-                }
-            }
+            en: './en.json',
+            cs: './cs.json'
         }
     }
 });
