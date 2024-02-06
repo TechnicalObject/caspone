ce2ce3fda1bbc4b622a39345546ed635e4dcbb7d
Luboš Komjati <79003016+TechnicalObject@users.noreply.github.com>
2024-02-06 21:41:05 +0100

    file init

---
+++ GTM.js
@@ -0,0 +1,25 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { window.dataLayer.push(arguments); }
+gtag('consent', 'default', {
+    ad_storage: 'denied',
+    analytics_storage: 'denied',
+    ad_user_data: 'denied',
+    ad_personalization: 'denied',
+    wait_for_update: 500
+});
+
+// load GTM
+const envPartTest = document.currentScript.getAttribute('env') || '';
+const gtmId = document.currentScript.getAttribute('gtmid') || '';
+const domainPrefix = document.currentScript.getAttribute('domainprefix') || '';
+let envPart = '';
+const hostName = window.location.host;
+const found = hostName.includes(domainPrefix);
+if (found) {
+    envPart = envPartTest;
+}
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl+ envPart;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer',gtmId);
