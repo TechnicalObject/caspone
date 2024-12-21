9a9972d2301da03cb3d26201d7f638f319651690
Luboš Komjati <dev@casp.cz>
2024-12-21 01:54:07 +0100

    isStaging broader identification

--- GTM.js
+++ GTM.js
@@ -20,7 +20,7 @@ gtag('consent', 'default', {
 const gtmId = document.currentScript.getAttribute('gtmid') || '';
 const gtmUrl = document.currentScript.getAttribute('gtmurl') || 'https://www.googletagmanager.com/gtm.js';
 const currentHostName = window.location.host;
-const isStaging = currentHostName.includes('.local') || currentHostName.includes('-stg');
+const isStaging = currentHostName.includes('local') || currentHostName.includes('-stg');
 console.debug('isStaging', isStaging);
 const envPart = isStaging ? document.currentScript?.getAttribute('env') || '' : '';
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
